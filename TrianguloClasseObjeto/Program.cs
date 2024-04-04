56f397c2909bc938abe15d56f55c0cad8971f5d3
Evandro2270 <150724259+Evandro2270@users.noreply.github.com>
2024-04-04 16:21:28 -0300

    Adicionar arquivos de projeto.

---
+++ TrianguloClasseObjeto/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Globalization;
+using TrinaguloClasse;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                Triangulo x, y;
+                x = new Triangulo();
+                y = new Triangulo();
+
+                Console.WriteLine("Entre com as medidas do triangulo X:");
+                x.A = double.Parse(Console.ReadLine(), CI);
+                x.B = double.Parse(Console.ReadLine(), CI);
+                x.C = double.Parse(Console.ReadLine(), CI);
+
+                Console.WriteLine("Entre com as medidas do triangulo Y:");
+                y.A = double.Parse(Console.ReadLine(), CI);
+                y.B = double.Parse(Console.ReadLine(), CI);
+                y.C = double.Parse(Console.ReadLine(), CI);
+
+
+                double areaX = x.Area();
+
+                double areaY = y.Area();
+
+                Console.WriteLine("Area de X = " + areaX.ToString("F4", CI));
+                Console.WriteLine("Area de Y = " + areaY.ToString("F4", CI));
+
+                if (areaX > areaY)
+                {
+                    Console.WriteLine("Maior area: X");
+                }
+                Console.WriteLine("Maior area: Y");
+            }
+        }
+    }
+}
